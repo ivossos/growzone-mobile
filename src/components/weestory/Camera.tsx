7c949d5040a88a41f84ee05a9f646be3f9e711a5
Vitor Cayres <vitor.cayres>
2025-04-01 22:00:00 -0300

    feat(weestory): update screen camera

--- src/components/weestory/Camera.tsx
+++ src/components/weestory/Camera.tsx
@@ -1,103 +1,164 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import {
   View,
   Text,
-  Modal,
   TouchableOpacity,
+  Image,
+  Modal,
   Button,
+  ActivityIndicator,
   StyleSheet,
 } from "react-native";
 import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
-
+import { ResizeMode, Video } from "expo-av";
+import { Ionicons } from "@expo/vector-icons";
 import { useCameraModal } from "@/context/camera-modal-context";
+import RevertCameraIcon from "@/assets/icons/revert-icon.svg";
 
-const CameraModal = () => {
+const CameraScreen = () => {
   const { isVisible, closeCamera } = useCameraModal();
-  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-  const [photo, setPhoto] = useState<string | null>(null);
-
-  const [facing, setFacing] = useState<CameraType>("back");
   const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef<any>(null);
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [media, setMedia] = useState<{
+    uri: string;
+    type: "photo" | "video";
+  } | null>(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      setLoading(true);
+      const photo = await cameraRef.current.takePictureAsync();
+      setMedia({ uri: photo.uri, type: "photo" });
+      setLoading(false);
+    }
+  };
+
+  const recordVideo = async () => {
+    if (cameraRef.current && !isRecording) {
+      setIsRecording(true);
+      const video = await cameraRef.current.recordAsync();
+      setMedia({ uri: video.uri, type: "video" });
+      setIsRecording(false);
+    }
+  };
+
+  const stopRecording = async () => {
+    if (cameraRef.current && isRecording) {
+      cameraRef.current.stopRecording();
+    }
+  };
+
+  const resetMedia = () => setMedia(null);
 
   function toggleCameraFacing() {
     setFacing((current) => (current === "back" ? "front" : "back"));
   }
 
+  console.log("logger", media);
+
   return (
     <Modal visible={isVisible} animationType="slide" transparent>
-      <View style={styles.container}>
-        {!permission && <View />}
+      <View style={styles.header}>
+        <TouchableOpacity onPress={closeCamera}>
+          <Ionicons name="close" size={35} color="white" />
+        </TouchableOpacity>
+      </View>
 
-        {!permission?.granted && (
-          <View style={styles.container}>
-            <Text style={styles.message}>
-              We need your permission to show the camera
-            </Text>
-            <Button onPress={requestPermission} title="grant permission" />
-          </View>
-        )}
+      {!permission && <View />}
 
-        {permission?.granted && (
-          <View style={styles.container}>
-            <CameraView style={styles.camera} facing={facing}>
-              <View style={styles.buttonContainer}>
+      {!permission?.granted && (
+        <View>
+          <Text>We need your permission to show the camera</Text>
+          <Button onPress={requestPermission} title="grant permission" />
+        </View>
+      )}
+
+      {permission?.granted && (
+        <View className="flex-1 bg-black-100">
+          {media ? (
+            <View className="flex-1 justify-center items-center">
+              {media.type === "photo" ? (
+                <Image source={{ uri: media.uri }} className="w-full h-4/5" />
+              ) : (
+                <Video
+                  source={{ uri: media.uri }}
+                  className="w-full h-4/5"
+                  useNativeControls
+                  resizeMode={ResizeMode.COVER}
+                  isLooping
+                />
+              )}
+              <View className="flex-row justify-between w-full p-4 absolute bottom-5">
                 <TouchableOpacity
-                  style={styles.button}
-                  onPress={toggleCameraFacing}
+                  onPress={resetMedia}
+                  className="bg-red-500 p-3 rounded-full"
                 >
-                  <Text style={styles.text}>Flip Camera</Text>
+                  <Ionicons name="close" size={30} color="white" />
                 </TouchableOpacity>
                 <TouchableOpacity
                   onPress={closeCamera}
-                  style={[styles.button, { backgroundColor: "red" }]}
+                  className="bg-green-500 p-3 rounded-full"
                 >
-                  <Text style={styles.buttonText}>Fechar</Text>
+                  <Ionicons name="checkmark" size={30} color="white" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          ) : (
+            <CameraView ref={cameraRef} facing={facing} style={styles.camera}>
+              <View style={styles.buttonContainer}>
+                <View style={{ height: 10 }} />
+                <TouchableOpacity
+                  onPressIn={recordVideo}
+                  onPressOut={stopRecording}
+                  onLongPress={recordVideo}
+                  onPress={takePicture}
+                  className="w-20 h-20 border-4 border-white rounded-full justify-center items-center"
+                >
+                  {loading ? (
+                    <ActivityIndicator color="#fff" />
+                  ) : isRecording ? (
+                    <View className="w-12 h-12 bg-red-500 rounded-lg" />
+                  ) : (
+                    <View className="w-16 h-16 bg-white rounded-full" />
+                  )}
+                </TouchableOpacity>
+                <TouchableOpacity onPress={toggleCameraFacing}>
+                  <RevertCameraIcon />
                 </TouchableOpacity>
               </View>
             </CameraView>
-          </View>
-        )}
-      </View>
+          )}
+        </View>
+      )}
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-  },
-  message: {
-    textAlign: "center",
-    paddingBottom: 10,
+  header: {
+    position: "absolute",
+    top: 50,
+    left: 20,
+    zIndex: 1,
   },
   camera: {
     flex: 1,
     width: "100%",
   },
   buttonContainer: {
-    flex: 1,
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    height: 130,
+    backgroundColor: "#000",
     flexDirection: "row",
-    backgroundColor: "transparent",
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: "flex-end",
+    justifyContent: "space-around",
     alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "white",
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
+    paddingBottom: 20,
   },
 });
 
-export default CameraModal;
+export default CameraScreen;
